509cd1d01dccf7ea88a04c11b9a0afef9e1d1d57
S-havl <pansillomcvalentincampostorres@gmail.com>
2026-06-02 10:41:29 -0500

    feat(TriangleVulkanTest): create a getRequiredExtensions function that will return the required list of extensions based on whether validation layers are enabled or not

--- TriangleVulkanTest/main.cpp
+++ TriangleVulkanTest/main.cpp
@@ -100,6 +100,20 @@ private:
         } 
     }
 
+    std::vector<const char*> getRequiredExtensions() {
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+        if (enableValidationLayers) {
+            extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+        }
+
+        return extensions;
+    }
+
     bool checkValidationLayerSupport() {
         uint32_t layerCount;
         vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
